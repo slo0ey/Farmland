68320c3e7a308f2d4818d57a45ca1a6ef791658e
slo0ey <rurchi1206@gmail.com>
2023-06-20 00:11:29 +0900

    플레이어 클래스 리팩토링

--- entity/player.py
+++ entity/player.py
@@ -1,27 +1,64 @@
 from util.image import load_sprite
 from util.physics import get_direction4
+from util.types import Position, Velocity
 
 import pygame
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, group, spawn_pos):
-        super().__init__(group)
-
+class PlayerEngine:
+    def __init__(
+            self,
+            start_pos: Position,
+            start_direction: pygame.math.Vector2 = pygame.math.Vector2(0, 1),
+            speed: int = 100,
+    ):
         # general setup
         self.status = 'stand'
 
-        # sprite setup
-        self.init_sprites()
-        self.image = self.__animations[self.status][0]
-        self.rect = self.image.get_rect(center=spawn_pos)
-        self.frame = 0
-
         # physics setup
-        self.vector = pygame.math.Vector2(0, 0)
-        self.direction = pygame.math.Vector2(0, 1)
-        self.position = pygame.math.Vector2(self.rect.center)
-        self.speed = 120
+        self.position = start_pos
+        self.direction = start_direction
+        self.speed = speed
+
+    def on_keypress(self, delta_time):
+        keys = pygame.key.get_pressed()
+
+        self.on_keypress_wasd(keys, delta_time)
+
+    # keypress events
+    def on_keypress_wasd(self, keys: pygame.key.ScancodeWrapper, delta_time):
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+        if self.direction.magnitude() > 0:
+            self.status = f"walk-{get_direction4(self.direction)}"
+            self.direction = self.direction.normalize()
+        else:
+            self.status = f"idle-{get_direction4(self.direction)}"
+
+        self.position += self.direction * self.speed * delta_time
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, group, engine: PlayerEngine):
+        super().__init__(group)
+        self.engine = engine
+
+        self.init_sprites()
+        self.image = self.__animations[self.engine.status][0]
+        self.rect = self.image.get_rect(center=engine.position)
+        self.frame = 0
 
     def init_sprites(self):
         self.__animations = {
@@ -40,47 +77,14 @@ class Player(pygame.sprite.Sprite):
             for sprite in load_sprite('entity', ['player', anim]):
                 self.__animations[anim].append(pygame.transform.scale(sprite, (96, 96)))
 
-    def input(self, dt):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
-            self.vector.y = -1
-        elif keys[pygame.K_DOWN]:
-            self.vector.y = 1
-        else:
-            self.vector.y = 0
-
-        if keys[pygame.K_LEFT]:
-            self.vector.x = -1
-        elif keys[pygame.K_RIGHT]:
-            self.vector.x = 1
-        else:
-            self.vector.x = 0
-
-    def update_state(self):
-        if self.vector.magnitude() > 0:
-            self.direction = self.vector.normalize()
-            self.status = f"walk-{get_direction4(self.direction)}"
-        else:
-            self.status = f"idle-{get_direction4(self.direction)}"
-
-    def move(self, dt):
-        if self.vector.magnitude() > 0:
-            self.vector = self.vector.normalize()
-        self.position += self.vector * self.speed * dt
-        self.rect.center = self.position
-
-    def animate(self, dt, speed=1):
-        total_frames = len(self.__animations[self.status])
-        self.frame += total_frames * dt * speed
+    def animate(self, delta_time, speed=1):
+        total_frames = len(self.__animations[self.engine.status])
+        self.frame += total_frames * delta_time * speed
 
         if self.frame >= total_frames:
             self.frame = 0
 
-        self.image = self.__animations[self.status][int(self.frame)]
+        self.image = self.__animations[self.engine.status][int(self.frame)]
 
-    def update(self, dt):
-        self.input(dt)
-        self.update_state()
-        self.move(dt)
-        self.animate(dt)
+    def update(self, delta_time):
+        self.engine.on_keypress(delta_time)
