bfcf8ba63ff0e432f8514ec1cf6fee0c555d2672
slo0ey <rurchi1206@gmail.com>
2023-06-24 23:13:46 +0900

    get_direction4 리턴값을 대문자로 바꿈

--- util/physics.py
+++ util/physics.py
@@ -4,13 +4,13 @@ import pygame
 def get_direction4(direction: pygame.math.Vector2):
     dir4 = ''
     if direction.y < 0:
-        dir4 = 'up'
+        dir4 = 'UP'
     elif direction.y > 0:
-        dir4 = 'down'
+        dir4 = 'DOWN'
 
     if direction.x < 0:
-        dir4 = 'left'
+        dir4 = 'LEFT'
     elif direction.x > 0:
-        dir4 = 'right'
+        dir4 = 'RIGHT'
 
     return dir4
