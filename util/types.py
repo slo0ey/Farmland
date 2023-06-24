b1ba69367c565ba43919ea1089c1da824f29a4fa
slo0ey <rurchi1206@gmail.com>
2023-06-24 23:14:38 +0900

    PlayerStatus의 각 value 타입을 str -> StatusInfo로 변경

--- util/types.py
+++ util/types.py
@@ -1,7 +1,16 @@
 from typing import *
 
-
 Size = Tuple[int, int]
 IntPosition = Tuple[int, int]
 Position = Tuple[float, float]
 Velocity = Tuple[float, float]
+
+
+class StatusInfo:
+    def __init__(
+            self,
+            alias: str,
+            animate_speed: float = 1.0
+    ):
+        self.alias = alias
+        self.animate_speed = animate_speed
