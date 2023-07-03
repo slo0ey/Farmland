b2d58044de91dc847491870b34121b03ee1a13cd
slo0ey <rurchi1206@gmail.com>
2023-07-04 00:00:12 +0900

    불필요한 리소스 파일들 제거

--- assets/map/plain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="plain" tilewidth="64" tileheight="64" tilecount="15" columns="15">
- <image source="../image/terrain/plain.png" width="960" height="64"/>
-</tileset>
